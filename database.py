d90e61d1ab562efec7d5c6d8d43ccf55f380fb6d
Ros <rostislavbondar3@gmail.com>
2026-06-03 14:19:47 +0200

    feat(database): change SQLAlchemy on async

--- database.py
+++ database.py
@@ -1,21 +1,21 @@
 from fastapi import Depends
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 from typing import Annotated
 from config import db_url
 
-engine = create_engine(db_url)
+engine = create_async_engine(db_url)
 
-SessionLocal =  sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal =  async_sessionmaker(engine, autoflush= False, autocommit= False,expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
 
-def get_db():
+async def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
 
-DBSession = Annotated[Session, Depends(get_db)]
+DBSession = Annotated[AsyncSession, Depends(get_db)]
