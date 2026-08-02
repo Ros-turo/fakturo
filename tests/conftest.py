87d9a62f6cef820fca470aedbaf7eea0e7d0ef94
Ros <rostislavbondar3@gmail.com>
2026-08-02 15:08:09 +0200

    hotfix(conftest): resolve bad merge, add new conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,81 @@
 import pytest
-from fastapi.testclient import TestClient
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy import event
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
+from settings import settings
 from main import app
+from database import Base, get_db
+from routers.auth import get_current_user
 
+
+@pytest.fixture(scope="session")
+async def engine():
+    engine = create_async_engine(url=settings.test_db_url)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield engine
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    await engine.dispose()
 
 @pytest.fixture(scope="function")
-def client():
-    return TestClient(app=app)
+async def db(engine):
+    async with engine.connect() as conn:
+        await conn.begin()
+        session = AsyncSession(bind=conn, expire_on_commit=False)
+        await conn.begin_nested()
+
+        @event.listens_for(session.sync_session, "after_transaction_end")
+        def restart_savepoint(sess, transaction):
+
+            if conn.closed:
+                return
+            if not conn.in_nested_transaction():
+                conn.sync_connection.begin_nested()
+
+
+        yield session
+        await session.close()
+        await conn.rollback()
+@pytest.fixture(scope="function")
+async def _base_client(db):
+    def get_override_db():
+        yield db
+    app.user_middleware = []
+    app.middleware_stack = None
+    app.dependency_overrides[get_db] = get_override_db
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url = "http://test"
+    ) as ac:
+        yield ac
+    app.dependency_overrides.clear()
+
+@pytest.fixture(scope="function")
+async def unauthorized_client(_base_client):
+    return _base_client
+
+@pytest.fixture(scope="function")
+async def client(_base_client):
+
+    user_data = {
+        "password": "stringst",
+        "email": "user@example.com",
+        "name": "string",
+        "surname": "string",
+        "ico": "14044984",
+        "dic": "SK22500300",
+        "city": "string",
+        "psc": "string",
+        "street": "string",
+        "house_number": "string"
+    }
+    response = await _base_client.post("/auth/register", json=user_data)
+    print(response.status_code)
+    print(response.text)
+    uid = response.json()['UID']
+    app.dependency_overrides[get_current_user] = lambda: {"uid": uid}
+    yield _base_client
+    app.dependency_overrides.clear()
+
