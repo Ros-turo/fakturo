cc54770f58ab771edad8db00e5425c4ba110d3fa
Ros <rostislavbondar3@gmail.com>
2026-09-26 00:48:34 +0200

    test(middleware): add MaxBodySizeMiddleware unit tests

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -3,8 +3,9 @@ import pytest
 from _pytest.logging import LogCaptureFixture
 from typing import Any, Callable
 import logging
-from middleware import CORSMiddleware, SlowRequestMiddleware
+from middleware import CORSMiddleware, SlowRequestMiddleware, MaxBodySizeMiddleware
 from settings import settings
+import json
 
 ALLOWED_ORIGIN = settings.allowed_origins[0]
 ALLOWED_ORIGIN_BYTES = ALLOWED_ORIGIN.encode()
@@ -38,7 +39,7 @@ async def fake_app() -> Callable:
     async def app_wrapper(scope: dict[str, Any], receive: Any, send: Callable) -> None:
         await asyncio.sleep(0.1)
         await send({"type": "http.response.start", "headers": [], "status": 200})
-        await send({"type": "http.response.body", "body": ""})
+        await send({"type": "http.response.body", "body": b""})
 
     return app_wrapper
 
@@ -159,3 +160,40 @@ async def test_cors_middleware_method_options(
     assert status_code == 200
     assert len(allow_origin_headers) == 1
     assert allow_origin_headers[0][1] == ALLOWED_ORIGIN_BYTES
+
+
+#MaxBodySizeMiddleware
+async def test_max_body_size_middleware_blocked_by_limit(
+        fake_app: Callable,
+        fake_scope: dict[str, Any],
+        fake_send: Callable,
+        messages_storage: list[Any],
+) -> None:
+
+    middleware = MaxBodySizeMiddleware(fake_app, max_body_size=1024)
+
+    fake_scope["headers"].append((b"content-length", b"1025"))
+    await middleware(fake_scope, receive=None, send=fake_send)
+
+    status_code = messages_storage[0].get("status")
+    json_body = messages_storage[1].get("body").decode()
+    body = json.loads(json_body)
+    detail = body["detail"]
+
+    assert status_code == 413
+    assert "too large" in detail.lower()
+
+async def test_max_body_size_middleware_under_limit(
+        fake_app: Callable,
+        fake_scope: dict[str, Any],
+        fake_send: Callable,
+        messages_storage: list[Any],
+) -> None:
+
+    middleware = MaxBodySizeMiddleware(fake_app, max_body_size=1024)
+
+    fake_scope["headers"].append((b"content-length", b"1022"))
+    await middleware(fake_scope, receive=None, send=fake_send)
+    status_code = messages_storage[0].get("status")
+
+    assert status_code == 200
